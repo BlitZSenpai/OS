9e53898438ba2d78e8985de6b4b834dec28d8650
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-21 22:10:45 +0530

    sjf algo

--- FCFS.c
+++ FCFS.c
@@ -5,14 +5,14 @@ int main()
   float wtavg, tatavg;
   printf("\n Enter the number of proceses:");
   scanf("%d", &n);
-  for (i = 0; i <= n; i++)
+  for (i = 0; i < n; i++)
   {
     printf("Enter the Burst Time for process %d :", i);
     scanf("%d", &bt);
   }
   wt[0] = wtavg = 0;
   tat[0] = tatavg = bt[0];
-  for (i = 0; i <= n; i++)
+  for (i = 0; i < n; i++)
   {
     wt[i] = wt[i - 1] + bt[i - 1];
     tat[i] = tat[i - 1] + bt[i];
@@ -20,9 +20,9 @@ int main()
     tatavg = tatavg + tat[i];
   }
   printf("\tPROCESS \tBURST TIME \tWAITING TIME \tTURNAROUND TIME \n");
-  for (i = 0; i <= n; i++)
-    printf("\n \t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
+  for (i = 0; i < n; i++)
+    printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
   printf("\n Average Waiting Time: %f", wtavg / n);
-  printf("\n Turnaround Time: %f", tatavg / n);
+  printf("\n Average Turnaround Time: %f", tatavg / n);
   return 0;
 }
